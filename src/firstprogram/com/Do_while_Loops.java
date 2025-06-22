15755fd13442871023a5b1517cdba6bf9906575e
Arpan-13 <arpanmaji1303@gmail.com>
2025-06-23 03:00:40 +0530

    third commit

---
+++ src/firstprogram/com/Do_while_Loops.java
@@ -0,0 +1,12 @@
+package firstprogram.com;
+
+public class Do_while_Loops {
+    public static void main(String[] args) {
+        int i=0;
+        do {
+            System.out.println(i);
+            i=i+1;
+        }
+        while(i<11);
+    }
+}
